379319b431ec7f1b0290aa4eec55e1c0d64c5500
Eric <zheng.eric.y@gmail.com>
2018-02-24 18:37:04 -0500

    finals done and ready for hsquizbowl upload

--- move.sh
+++ move.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # just to automate this and make my life easier
-mv playoffs_standings.html stats.html
-mv playoffs_individuals.html individuals.html
+mv combined_standings.html stats.html
+mv combined_individuals.html individuals.html
 
 echo 'now add the lines in README.md to the HTML files'
