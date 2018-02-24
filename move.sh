40041abeaad9f6756bda28e4825c559d21064032
Eric <zheng.eric.y@gmail.com>
2018-02-24 12:19:23 -0500

    round 4 done

---
+++ move.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# just to automate this and make my life easier
+mv prelims_standings.html stats.html
+mv prelims_individuals.html individuals.html
+
+echo 'now add the lines in README.md to the HTML files'
